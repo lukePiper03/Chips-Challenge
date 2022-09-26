fc3927c6e6126273e003051e2acf5d6005433924
Linda <zhanglind1@ecs.vuw.ac.nz>
2022-09-26 16:43:37 +1300

    Fixed issue: Exit no longer requires Level parameter

--- src/nz/ac/vuw/ecs/swen225/gp22/domain/Exit.java
+++ src/nz/ac/vuw/ecs/swen225/gp22/domain/Exit.java
@@ -5,16 +5,16 @@ import nz.ac.vuw.ecs.swen225.gp22.renderer.imgs.Img;
 import nz.ac.vuw.ecs.swen225.gp22.renderer.sounds.Sound;
 
 /**
- * Rrepresents an exit entity. Ends the game.
+ * Rrepresents an exit entity.
  * @author lindazhang
  *
  */
-record Exit(Point pos, Level level) implements Entity{
+record Exit(Point pos) implements Entity{
 	public void onInteraction(Player p, Cells cells, SoundPlayer soundplayer) {
 		if(!p.getPos().equals(pos)) throw new IllegalStateException("Player is not on Exit!");
 		p.entitiesToRemove().add(this);
 		soundplayer.play(Sound.beep);
-		level.gameOver(); //end the game
+		//level.gameOver(); //end the game
 	}
 	public Point getPos() {return pos;}
 	public Img getImage() {return Img.water;} //change later
