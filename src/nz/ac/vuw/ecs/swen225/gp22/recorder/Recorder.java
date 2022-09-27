81bf36c2a498323af7dd68890adeab60c1e3fed8
Quinten <50760069+qwsmit@users.noreply.github.com>
2022-09-27 23:44:14 +1300

    Save and load working

--- src/nz/ac/vuw/ecs/swen225/gp22/recorder/Recorder.java
+++ src/nz/ac/vuw/ecs/swen225/gp22/recorder/Recorder.java
@@ -1,37 +1,57 @@
 package nz.ac.vuw.ecs.swen225.gp22.recorder;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 
 import org.jdom2.Document;
 import org.jdom2.Element;
 import org.jdom2.JDOMException;
 import org.jdom2.input.SAXBuilder;
+import org.jdom2.output.Format;
+import org.jdom2.output.XMLOutputter;
 
 import nz.ac.vuw.ecs.swen225.gp22.app.Direction;
+import nz.ac.vuw.ecs.swen225.gp22.domain.Level;
 
+/**
+ * @author Quinten Smit 300584150
+ *
+ */
 public class Recorder {
 	Document document;
 	Element eventsElement;
+	
 	/**
-	 * @param mapFileName 
-	 * 
+	 * Makes a new recorder which is ready to receive inputs
+	 * @param level the current level
 	 */
-	public Recorder(String mapFileName) {
+	public Recorder(Level level) {
 		document = new Document();
 		Element replayElement = new Element("replay");
 		document.setRootElement(replayElement);
 		
 		Element mapElement = new Element("map");
 		//Maybe save the map as well here? Could be expanded later
-		mapElement.addContent(mapFileName);
+		//mapElement.setText(level.getFileName());
 		replayElement.addContent(mapElement);
 		
-		Element eventsElement = new Element("events");
+		eventsElement = new Element("events");
 		replayElement.addContent(eventsElement);
 	}
 	
 	/**
+	 * Loads a Recorder from a file
+	 * @param fileName The name of the file to load from
+	 * @throws IOException 
+	 * @throws JDOMException 
+	 */
+	public Recorder(String fileName) throws JDOMException, IOException {
+		document = (Document) (new SAXBuilder()).build(new File(fileName));
+	}
+	
+	/**
+	 * Adds a new player moved event to the recorder
 	 * @param time The time at which the action was performed
 	 * @param action The direction that the player moved in
 	 * @param length The length of time that the action was performed over
@@ -42,30 +62,67 @@ public class Recorder {
 		
 		//Could be used for non player actions
 		Element actorElement = new Element("actor");
-		actorElement.addContent("Player");
+		actorElement.setText("Player");
 		eventElement.addContent(actorElement);
 		
 		Element actionElement = new Element("action");
 		actionElement.setAttribute("length", length+"");
-		actionElement.addContent(action.toString());
+		actionElement.setText(action.toString());
 		eventElement.addContent(actionElement);
 		
 		eventsElement.addContent(eventElement);
 	}
 	
-	public void saveToFile(String name){
-		
+	/**
+	 * Saves the current Record to a file in the XML format
+	 * @param fileName the name of the file to save to
+	 * @throws IOException 
+	 */
+	public void saveToFile(String fileName) throws IOException{
+		new XMLOutputter(Format.getPrettyFormat()).output(document, new FileWriter(fileName+".xml"));
 	}
 	
+	public String toString() {
+		return new XMLOutputter(Format.getPrettyFormat()).outputString(document);
+	}
+	
+	
+	/*****************************************************************************
+	 * Testing methods
+	 *****************************************************************************/
+	
 	/**
-	 * 
+	 * Testing main, will create a Recorder, print it, save it to test.xml,
+	 * and then load it back into another Recorder from text.xml and print it out. 
+	 * @param args
 	 */
-	public static void readFromFile() {
-		try {
-			Element channelElement = ((Document) (new SAXBuilder()).build(new File(""))).getRootElement();
+	public static void main(String[] args){
+	    Recorder recorder1 = makeTestRecorder();
+	    System.out.println("One: " + recorder1.toString());
+	    
+	    try {
+			recorder1.saveToFile("test");
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+	    
+	    Recorder recorder2 = null;
+	    try {
+	    	recorder2 = new Recorder("test.xml");
 		} catch (JDOMException | IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	    
+	    System.out.println("Two " + recorder2.toString());
 	}
+	
+	private static Recorder makeTestRecorder() {
+		Recorder recorder = new Recorder((Level)null);
+	    recorder.savePlayerMoveEvent(0, Direction.Left, 10);
+	    recorder.savePlayerMoveEvent(5, Direction.Right, 13);
+	    return recorder;
+	}
+	
 }
