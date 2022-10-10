ed6c3071601b307c55fb25f6cf1677580d466da3
Declan Cross <crossdecl@ecs.vuw.ac.nz>
2022-10-11 11:48:11 +1300

    Finished checkstyle

--- src/nz/ac/vuw/ecs/swen225/gp22/renderer/EventHandler.java
+++ src/nz/ac/vuw/ecs/swen225/gp22/renderer/EventHandler.java
@@ -1,56 +1,59 @@
 package nz.ac.vuw.ecs.swen225.gp22.renderer;
 
-import nz.ac.vuw.ecs.swen225.gp22.domain.Level;
-import nz.ac.vuw.ecs.swen225.gp22.renderer.sounds.Sound;
-
 import java.util.ArrayList;
 import java.util.List;
-
-import nz.ac.vuw.ecs.swen225.gp22.domain.*;
+import nz.ac.vuw.ecs.swen225.gp22.domain.Level;
+import nz.ac.vuw.ecs.swen225.gp22.domain.Observer;
+import nz.ac.vuw.ecs.swen225.gp22.domain.Subject;
+import nz.ac.vuw.ecs.swen225.gp22.renderer.sounds.Sound;
 
 
 /**
- * Class that observes subjects in domain
+ * Class that observes subjects in domain.
+ *
  * @author Declan Cross 300567901
  */
-public class EventHandler{
-	// active listeners and sound player variables
-	List<Subject> activeListeners = new ArrayList<>();
-	SoundPlayer s;
-	
-	/**
-	 * Constructor
-	 * @param l level to get objects to observe
-	 * @param s SoundPlayer object to generate sound effects 
-	 */
-	public EventHandler(Level l, SoundPlayer s){
-		this.s = s;
-		// add beep sound to all entity interactions
-		l.getEntites().forEach(a -> {
-			a.attach(()->s.play(Sound.beep));
-			activeListeners.add(a);
-		});
-		// walking sound effect for player
-		//l.getPlayer().attach(()->s.play(Sound.beep)); activeListeners.add(l.getPlayer());
-		
-	}
-	
-	/**
-	 * Attach an listener observer to a subject
-	 * @param s subject
-	 * @param o observer with onChange lambda method
-	 */
-	public void attach(Subject s, Observer o) {
-		s.attach(o);
-		activeListeners.add(s);
-	}
-	
-	/**
-	 * Removes all observers for the level
-	 */
-	public void detatchAll() {
-		activeListeners.forEach(a -> a.detatch());
-		activeListeners.clear();
-	}
+public class EventHandler {
+  // active listeners and sound player variables
+  List<Subject> activeListeners = new ArrayList<>();
+  SoundPlayer sounds;
+
+  /**
+   * Constructor.
+   *
+   * @param l level to get objects to observe
+   * @param s SoundPlayer object to generate sound effects
+   */
+  public EventHandler(Level l, SoundPlayer s) {
+    this.sounds = s;
+    // add beep sound to all entity interactions
+    l.getEntites().forEach(a -> {
+      a.attach(() -> s.play(Sound.beep));
+      activeListeners.add(a);
+    });
+    // walking sound effect for player
+    // l.getPlayer().attach(()->s.play(Sound.beep));
+    // activeListeners.add(l.getPlayer());
+
+  }
+
+  /**
+   * Attach an listener observer to a subject.
+   *
+   * @param s subject
+   * @param o observer with onChange lambda method
+   */
+  public void attach(Subject s, Observer o) {
+    s.attach(o);
+    activeListeners.add(s);
+  }
+
+  /**
+   * Removes all observers for the level.
+   */
+  public void detatchAll() {
+    activeListeners.forEach(a -> a.detatch());
+    activeListeners.clear();
+  }
 
 }
--- src/nz/ac/vuw/ecs/swen225/gp22/renderer/SoundPlayer.java
+++ src/nz/ac/vuw/ecs/swen225/gp22/renderer/SoundPlayer.java
@@ -9,107 +9,116 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /**
- * SoundPlayer class to keep track of ongoing and loaded sounds for playing and stopping
+ * SoundPlayer class to keep track of ongoing and loaded sounds for playing and
+ * stopping.
+ *
  * @author Declan Cross 300567901
  */
-public class SoundPlayer
-{
-	// loaded map of sound clips
-	Map<Sound, Clip> sounds;
-	
-    /**
-     * SoundPlayer constructor that loads all files
-     */
-    public SoundPlayer()
-    {
-    	// load in all sounds and add to map for retrieval
-    	sounds = new HashMap<>();
-    	sounds.putAll(Stream.of(Sound.values()).collect(Collectors.toMap(a->a, a->a.clip)));
+public class SoundPlayer {
+  // loaded map of sound clips
+  Map<Sound, Clip> sounds;
+
+  /**
+   * SoundPlayer constructor that loads all files.
+   */
+  public SoundPlayer() {
+    // load in all sounds and add to map for retrieval
+    sounds = new HashMap<>();
+    sounds.putAll(Stream.of(Sound.values()).collect(Collectors.toMap(a -> a, a -> a.clip)));
+  }
+
+  /**
+   * Returns a sound clip for usage by other functions.
+   *
+   * @param s sound name to retrieve
+   * @return sound clip
+   */
+  private Clip getSound(Sound s) {
+    if (sounds.containsKey(s)) {
+      return sounds.get(s);
+    } else {
+      throw new IllegalArgumentException("Sound " + s + " does not exist.");
     }
-    
-    /**
-     * Returns a sound clip for usage by other functions
-     * @param s  sound name to retrieve
-     * @return   sound clip
-     */
-    private Clip getSound(Sound s) {
-    	if(sounds.containsKey(s)) {
-    		return sounds.get(s);
-    	} else {
-    		throw new IllegalArgumentException("Sound " + s + " does not exist.");
-    	}
-    }
-    
-    /**
-     * Plays a requested sound
-     * @param s  name of sound
-     */
-    public void play(Sound s){
-    	// take argument to play specific sound
-    	System.out.println("Playing sound " + s);
-    	Clip curClip = getSound(s);
-    	curClip.setFramePosition(0);
-    	curClip.start();
-    }
-    
-    /**
-     * Loops a requested sound with a fade in effect
-     * @param s   name of sound 
-     * @param maxVol   maximum fade in volume
-     */
-    public void loop(Sound s, int maxVol){
-    	System.out.println("Looping sound " + s);
-    	// get sound control and set to min volume
-    	FloatControl gainControl = (FloatControl) getSound(s).getControl(FloatControl.Type.MASTER_GAIN);        
-        gainControl.setValue(20f * (float) Math.log10(0.01));
-        // set song to loop mode
+  }
+
+  /**
+   * Plays a requested sound.
+   *
+   * @param s name of sound
+   */
+  public void play(Sound s) {
+    // take argument to play specific sound
+    System.out.println("Playing sound " + s);
+    Clip curClip = getSound(s);
+    curClip.setFramePosition(0);
+    curClip.start();
+  }
+
+  /**
+   * Loops a requested sound with a fade in effect.
+   *
+   * @param s      name of sound
+   * @param maxVol maximum fade in volume
+   */
+  public void loop(Sound s, int maxVol) {
+    System.out.println("Looping sound " + s);
+    // get sound control and set to min volume
+    FloatControl gainControl = (FloatControl) getSound(s).getControl(FloatControl.Type.MASTER_GAIN);
+    gainControl.setValue(20f * (float) Math.log10(0.01));
+    // set song to loop mode
+    getSound(s).loop(Clip.LOOP_CONTINUOUSLY);
+    // loop to gradually fade in sound
+    for (double i = 0; i < maxVol; i++) {
+      try {
+        Thread.sleep(45);
+        gainControl.setValue(20f * (float) Math.log10(i / 100));
         getSound(s).loop(Clip.LOOP_CONTINUOUSLY);
-        // loop to gradually fade in sound
-        for(double i = 0; i<maxVol; i++) {
-        	try {
-        		Thread.sleep(45);
-        		gainControl.setValue(20f * (float) Math.log10(i/100));
-        		getSound(s).loop(Clip.LOOP_CONTINUOUSLY);
-			} catch (InterruptedException e) {e.printStackTrace();}
-        }
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
     }
-    
-    /**
-     * Fades a sound out then stops playing it
-     * @param s    name of sound
-     * @param startVol    initial volume
-     */
-    public void fadeOut(Sound s, int startVol){        
-    	System.out.println("Fade out sound " + s);
-    	// get sound control
-    	FloatControl gainControl = (FloatControl) getSound(s).getControl(FloatControl.Type.MASTER_GAIN);
-    	// loop to gradually fade out sound
-        for(double i = startVol; i>-100; i-=5) {
-        	try { 
-        		Thread.sleep(50);
-        		gainControl.setValue(20f * (float) Math.log10(i/100));
-				
-			} catch (InterruptedException e) {e.printStackTrace();}
-        }
-        // stop sound
-        getSound(s).stop();
+  }
+
+  /**
+   * Fades a sound out then stops playing it.
+   *
+   * @param s        name of sound
+   * @param startVol initial volume
+   */
+  public void fadeOut(Sound s, int startVol) {
+    System.out.println("Fade out sound " + s);
+    // get sound control
+    FloatControl gainControl = (FloatControl) getSound(s).getControl(FloatControl.Type.MASTER_GAIN);
+    // loop to gradually fade out sound
+    for (double i = startVol; i > -100; i -= 5) {
+      try {
+        Thread.sleep(50);
+        gainControl.setValue(20f * (float) Math.log10(i / 100));
+
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
     }
-  
-    /**
-     * Stops a sound 
-     * @param s  name of sound
-     */
-    public void stop(Sound s){
-    	System.out.println("Stopping sound " + s);
-    	getSound(s).stop();
-    }
-    
-    /**
-     * Stops all sounds
-     */
-    public void stopAll(){
-    	System.out.println("Stopping all");
-    	// loop through each sound and end
-    	sounds.values().stream().forEach(clip -> clip.stop());
-    }
+    // stop sound
+    getSound(s).stop();
+  }
+
+  /**
+   * Stops a sound .
+   *
+   * @param s name of sound
+   */
+  public void stop(Sound s) {
+    System.out.println("Stopping sound " + s);
+    getSound(s).stop();
+  }
+
+  /**
+   * Stops all sounds.
+   */
+  public void stopAll() {
+    System.out.println("Stopping all");
+    // loop through each sound and end
+    sounds.values().stream().forEach(clip -> clip.stop());
+  }
 }
