eb46da2cd3a955438b02a3dc3ae2320c00ddfb00
Luke Piper <piperluke@ecs.vuw.ac.nz>
2022-10-13 16:38:43 +1300

    Added checkstyle formatting to app module

--- src/nz/ac/vuw/ecs/swen225/gp22/app/CustomButton.java
+++ src/nz/ac/vuw/ecs/swen225/gp22/app/CustomButton.java
@@ -2,24 +2,29 @@ package nz.ac.vuw.ecs.swen225.gp22.app;
 
 import java.awt.Color;
 import java.awt.Cursor;
-
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
-
 import nz.ac.vuw.ecs.swen225.gp22.renderer.fonts.LoadedFont;
 
 /**
+ * Constructor for custom buttons with GUI design.
+ *
  * @author lukepiper
  *
  */
-public class CustomButton extends JButton{
-	CustomButton(String text) {
-		this.setText(text);
-		this.setCursor(new Cursor(Cursor.HAND_CURSOR));
-		this.setFont(LoadedFont.PixeloidSans.getSize(14f));
-		this.setForeground(Color.white);
-		this.setBackground(new Color(110, 74, 47, 150));
-		this.setBorder(BorderFactory.createLineBorder(Color.white, 4));
-		this.setOpaque(true);
-	}
+public class CustomButton extends JButton {
+  /**
+   * serialVersionUID.
+   */
+  private static final long serialVersionUID = 1L;
+
+  CustomButton(String text) {
+    this.setText(text);
+    this.setCursor(new Cursor(Cursor.HAND_CURSOR));
+    this.setFont(LoadedFont.PixeloidSans.getSize(14f));
+    this.setForeground(Color.white);
+    this.setBackground(new Color(110, 74, 47, 150));
+    this.setBorder(BorderFactory.createLineBorder(Color.white, 4));
+    this.setOpaque(true);
+  }
 }
