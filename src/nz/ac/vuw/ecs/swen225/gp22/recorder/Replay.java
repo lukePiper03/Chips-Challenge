d3834c82188959de8db25f939f48096301b8e913
Quinten <50760069+qwsmit@users.noreply.github.com>
2022-10-13 17:13:17 +1300

    Make Replay JPanel and allow it to control replays

--- src/nz/ac/vuw/ecs/swen225/gp22/recorder/Replay.java
+++ src/nz/ac/vuw/ecs/swen225/gp22/recorder/Replay.java
@@ -7,6 +7,12 @@ import java.util.Optional;
 import java.util.function.Consumer;
 import java.util.function.Predicate;
 
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.JSlider;
+import javax.swing.SwingUtilities;
+import javax.swing.Timer;
+
 import org.jdom2.Document;
 import org.jdom2.Element;
 import org.jdom2.JDOMException;
@@ -20,10 +26,18 @@ import nz.ac.vuw.ecs.swen225.gp22.global.Direction;
  * @author Quinten Smit 300584150
  *
  */
-public class Replay {
+public class Replay extends JPanel{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1874411741562700245L;
 	Element replayElement;
-	double time;
+	int time;
+	int speed = 33;
+	boolean end;
 	Consumer<Direction> movePlayer;
+	Runnable advanceByTick;
+	Timer timer;
 	
 	
 	/**
@@ -36,6 +50,21 @@ public class Replay {
 		replayElement = ((Document) (new SAXBuilder()).build(new File(fileName))).getRootElement();
 		time = Integer.parseInt(replayElement.getChild("time").getText());
 		//Element eventsElement = replayElement.getChild("events");
+		JButton step = new JButton("Advance By Step");
+		step.addActionListener(e->advanceByTick());
+		
+		JButton autoReplay = new JButton("Auto Replay");
+		autoReplay.addActionListener(e->startRunning());
+		
+		JSlider replaySpeed = new JSlider(0,10, speed);
+		replaySpeed.addChangeListener((e)->timer.setDelay(speed=((JSlider)e.getSource()).getValue()));
+		
+		JButton stop = new JButton("Stop");
+		stop.addActionListener(e->timer.stop());
+		
+		add(step);
+		add(autoReplay);
+		add(replaySpeed);
 	}
 	
 	/**
@@ -43,6 +72,29 @@ public class Replay {
 	 */
 	public void setController(Consumer<Direction> movePlayer) {
 		this.movePlayer = movePlayer;
+	}
+	
+	public void setAdvanceByTick(Runnable advanceByTick) {
+		this.advanceByTick = advanceByTick;
+	}
+	
+	public void advanceByTick() {
+		if (!end) {
+			advanceByTick.run();
+			movePlayer();
+			if (time >= findEndTime()) {
+				end = true;
+				timer.stop();
+			}
+		}
+	}
+	
+	public void startRunning() {
+		timer = new Timer(100-speed,unused->{
+		      assert SwingUtilities.isEventDispatchThread();
+		      advanceByTick();
+		});
+		timer.start();
 	}
 	
 	/**
@@ -60,7 +112,7 @@ public class Replay {
 	 * @return  if the move was stored in the recorder
 	 * 
 	 */
-	public boolean movePlayer() {
+	private boolean movePlayer() {
 		List<Element> eventList = replayElement.getChild("events").getChildren("events");
 		Optional<Element> event = eventList.stream()
 										   .filter((e)->(Integer.parseInt(e.getAttribute("time").getValue()) == time))
@@ -71,5 +123,10 @@ public class Replay {
 		movePlayer.accept(direction);
 		time++;
 		return true;
+	}
+	
+	private int findEndTime() {
+		List<Element> eventList = replayElement.getChild("events").getChildren("events");
+		return Integer.parseInt(eventList.get(eventList.size()-1).getAttributeValue("time"));
 	}
 }
